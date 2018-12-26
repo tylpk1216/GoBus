8afdfcfe607cf5827f0e9ab409452fc8bd6b2fa4
MichaelKuo <MichaelKuo@via.com.tw>
2018-12-26 17:36:00 +0800

    Use Python 3.6 to fix wx._core import error.

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Python37-32\Scripts\pyinstaller.exe -F -w -D -i GoBus.ico -n GoBus.exe --onefile main.py
+C:\Python36-32\Scripts\pyinstaller.exe --clean -D -F -w -i GoBus.ico -n GoBus.exe main.py
